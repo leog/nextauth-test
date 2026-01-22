71b8ed714878a7f135bd53dd316d0ae5e9655f8c
Leo Giovanetti <hello@leog.me>
2026-01-22 16:47:30 -0300

    Using custom domain

--- auth.ts
+++ auth.ts
@@ -28,7 +28,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         sameSite: "lax",
         secure: true,
         path: "/",
-        domain: ".vercel.app",
+        domain: ".urirep.com",
       },
     },
   },
